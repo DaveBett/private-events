56721990f6f45807eaabe3916d1eac5eac05e9cc
DaveBett <davide.betterini@gmail.com>
2026-03-24 11:30:48 +0000

    modified index view and fixed sign out button

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   resources :events
-  resources :users
+  resources :users, only: [ :show ]
   devise_for :users, path: "auth", path_names: { sign_in: "login", sign_out: "logout", password: "secret", confirmation: "verification", unlock: "unblock", registration: "register", sign_up: "cmon_let_me_in" }
 
   root "events#index"
