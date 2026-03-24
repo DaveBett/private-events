4f3b41154d0d40113aba6a01ca612e62f4bf4dd4
DaveBett <davide.betterini@gmail.com>
2026-03-24 11:56:37 +0000

    Added show view to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+      if @event.save
+        redirect_to @event
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :name, :location, :date ])
+  end
 end
