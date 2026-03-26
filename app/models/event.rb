a8303389d9d585ed774fe89b1c9cf75094f7e6aa
DaveBett <davide.betterini@gmail.com>
2026-03-26 11:18:30 +0000

    Separate event list in index view into upcoming and previous events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
 
   has_many :attendances, foreign_key: :attended_event_id, class_name: "Attendance"
   has_many :attendees, through: :attendances, source: :attendee
+
+
+  scope :future, -> { where("date >= ?", Time.now) }
+  scope :past, -> { where("date < ?", Time.now) }
 end
